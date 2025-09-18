cf96ebfea722aee7ddb750ed13febfe2f722a6cc
Tomás Carruço <tomascarruco.wk@gmail.com>
2025-09-18 16:58:15 +0100

    feat(wip): toggle options and base layout for settings

--- lib/pages/settings.dart
+++ lib/pages/settings.dart
@@ -1,27 +1,160 @@
 import 'package:flutter/material.dart';
 import 'package:greenly/components/base_layout.dart';
 
-import 'package:greenly/components/emission_monthly_delta.dart';
-import 'package:greenly/components/forum_discussion_thread.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var textTheme = TextTheme.of(context);
+
+    const scrollViewEdgeInsets = EdgeInsets.symmetric(
+      horizontal: 12,
+      vertical: 12,
+    );
+
     return BasePageLayout(
       floatingBtnIcon: Icons.add,
       floatingBtnTooltip: 'Available Actions',
       // --- Body
-      body: SingleChildScrollView(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      body: DefaultTabController(
+        // padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+        length: 3,
         child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [],
+          children: [
+            TabBar(
+              tabs: <Widget>[
+                Tab(icon: Icon(Icons.settings_outlined)),
+                Tab(icon: Icon(Icons.commute)),
+                Tab(icon: Icon(Icons.person_outlined)),
+              ],
+            ),
+            Expanded(
+              child: Padding(
+                padding: scrollViewEdgeInsets,
+                child: TabBarView(
+                  children: [
+                    SingleChildScrollView(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Column(
+                        spacing: 6,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.stretch,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            spacing: 4,
+                            children: [
+                              Text(
+                                'Base Settings',
+                                style: textTheme.titleMedium,
+                              ),
+                              Text(
+                                'Notifications and functionality.',
+                                style: textTheme.bodyMedium,
+                              ),
+                            ],
+                          ),
+                          Container(),
+                          Column(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              ToggleOption(
+                                optionIcon: Icons.notifications_rounded,
+                                active: true,
+                                title: 'Receive Notifications',
+                                description:
+                                    'Set to OFF sends only essential notifications.',
+                              ),
+                              ToggleOption(
+                                optionIcon: Icons.calendar_month_rounded,
+                                active: false,
+                                title: 'Reminders',
+                                description: 'Helps you achieve your goals.',
+                              ),
+                              ToggleOption(
+                                optionIcon: Icons.gamepad_rounded,
+                                active: true,
+                                action: (bool newValue) {},
+                                title: 'Ai Gamification',
+                                description:
+                                    'Gamify your tasks, and progress tracking.',
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    SingleChildScrollView(child: const Text('AAA')),
+                    SingleChildScrollView(child: const Text('AAA')),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
       hasActionButton: false,
+    );
+  }
+}
+
+class ToggleOption extends StatelessWidget {
+  final IconData optionIcon;
+  final bool active;
+  final String title;
+  final String? description;
+  final void Function(bool)? action;
+
+  const ToggleOption({
+    required this.optionIcon,
+    required this.active,
+    required this.title,
+    super.key,
+    this.description,
+    this.action,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    var textTheme = TextTheme.of(context);
+
+    var cardShape = RoundedRectangleBorder(
+      side: BorderSide(
+        color: Colors.grey.shade100,
+        width: 0,
+        style: BorderStyle.solid,
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(12)),
+    );
+
+    return Card(
+      shape: cardShape,
+      color: Colors.white,
+      elevation: 1,
+      child: Padding(
+        padding: EdgeInsetsGeometry.all(18),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          spacing: 10,
+          children: [
+            Icon(optionIcon, size: 28),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(title, style: textTheme.titleSmall),
+                  description != null
+                      ? Text(description!, style: textTheme.bodySmall)
+                      : Container(),
+                ],
+              ),
+            ),
+            Switch(value: active, onChanged: action),
+          ],
+        ),
+      ),
     );
   }
 }
